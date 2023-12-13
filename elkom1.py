441f69ff742e5abb9df0f4d5360b2315460d49d1
DonyFatahillah <145980954+DonyFatahillah@users.noreply.github.com>
2023-12-13 07:53:16 +0700

    Create elkom1.py

---
+++ elkom1.py
@@ -0,0 +1,16 @@
+def banner():
+    print("\n \n")
+
+    print("@@@@   @@@@@   @@  @  @   @")
+    print("@   @  @   @   @ @ @   @ @")
+    print("@   @  @   @   @  @@    @")
+    print("@@@@   @@@@@   @   @    @")
+
+    print("\n \n")
+banner()
+print("=====Elkom 1=====")
+a = [1 ,2 ,3 ,4, 1, 44]
+print(a)
+print("hasil reverse ke tuple")
+b = tuple(a)
+print(b)
